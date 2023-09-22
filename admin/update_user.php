54342d0c2770d9bb3d53a19058924c966ef569e2
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-09-22 12:55:23 +0300

    delete and update user from admin panel

---
+++ admin/update_user.php
@@ -0,0 +1,122 @@
+<?php $customTitle = "Update User"; ?>
+<?php include "pages/_header.php" ?>
+<?php include "pages/_navbar.php" ?>
+<?php include "../database/db_connection.php"; ?>
+
+<?php
+
+
+if (isset($_POST["update_userbtn"])) {
+    $user_id = $_POST["user_id"];
+    $fullname = $_POST["fullname"];
+    $email = $_POST["email"];
+    $user_role = $_POST["user_role"];
+
+    $updateQuery = "UPDATE users SET fullname = ?, email = ?, user_role = ? WHERE id = ?";
+    $stmt = mysqli_prepare($connection, $updateQuery);
+
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "sssi", $fullname, $email, $user_role, $user_id);
+        if (mysqli_stmt_execute($stmt)) {
+            header("Location: users_setting.php");
+            exit();
+        } else {
+            echo "Error updating user: " . mysqli_error($connection);
+        }
+
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "Error creating prepared statement: " . mysqli_error($connection);
+    }
+}
+?>
+
+
+
+
+
+
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <?php
+            if (isset($_POST["edit_userbtn"])) {
+
+                $user_id = $_POST["user_id"];
+
+
+                $editQuery = "SELECT * FROM users WHERE id = ?";
+                $stmt = mysqli_prepare($connection, $editQuery);
+
+                if ($stmt) {
+                    mysqli_stmt_bind_param($stmt, "i", $user_id);
+
+                    if (mysqli_stmt_execute($stmt)) {
+                        $result = mysqli_stmt_get_result($stmt);
+
+                        if ($user = mysqli_fetch_assoc($result)) {
+            ?>
+
+                            <div class="card">
+                                <div class="card-header">
+                                    <h2>Edit User: <?php echo $user['fullname'] ?> </h2>
+                                </div>
+                                <div class="card-body">
+                                    <form action="" method="post">
+                                        <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
+                                        <div class="form-group">
+                                            <label for="fullname">Full Name:</label>
+                                            <input type="text" class="form-control" name="fullname" value="<?php echo $user['fullname']; ?>"><br>
+                                        </div>
+
+                                        <div class="form-group ">
+                                            <label for="email">Email:</label>
+                                            <input type="text" class="form-control" name="email" value="<?php echo $user['email']; ?>"><br>
+                                        </div>
+                                        <div class="form-group ">
+                                            <label for="user_role">Role:</label>
+                                            <input type="text" class="form-control" name="user_role" value="<?php echo $user['user_role']; ?>"><br>
+                                        </div>
+
+
+                                </div>
+                                <div class="card-footer">
+                                    <a href="users_setting.php" class="btn btn-danger">Cancel</a>
+                                    <button type="submit" name="update_userbtn" class="btn btn-primary">Update</button>
+                                    </form>
+                                </div>
+                            </div>
+
+            <?php
+                        } else {
+                            echo "User not found.";
+                        }
+                    } else {
+                        echo "Error executing query: " . mysqli_error($connection);
+                    }
+
+                    mysqli_stmt_close($stmt);
+                } else {
+                    echo "Error creating prepared statement: " . mysqli_error($connection);
+                }
+            }
+
+            ?>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<?php include "pages/_footer.php" ?>
---
+++ admin/users_setting.php
@@ -0,0 +1,105 @@
+<?php $customTitle = "Page Name"; ?>
+<?php include "pages/_header.php" ?>
+<?php include "pages/_navbar.php" ?>
+<?php include "../database/db_connection.php"; ?>
+
+<?php
+if (isset($_POST["delete_userbtn"])) {
+    $user_id = $_POST["user_id"];
+
+    $deleteQuery = "DELETE FROM users WHERE id = ?";
+    $stmt = mysqli_prepare($connection, $deleteQuery);
+
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "i", $user_id);
+
+        if (mysqli_stmt_execute($stmt)) {
+            header("Location: users_setting.php");
+            exit();
+        } else {
+            echo "Error deleting account: " . mysqli_error($connection);
+        }
+
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "Error creating prepared statement: " . mysqli_error($connection);
+    }
+}
+
+
+?>
+
+
+<?php
+$query = "SELECT * FROM users";
+$result = mysqli_query($connection, $query);
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h1>User List <button class="btn btn-primary float-end">Add New User</button></h1>
+                </div>
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Process</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            while ($user = mysqli_fetch_assoc($result)) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $user['id']; ?></td>
+                                    <td><?php echo $user['fullname']; ?></td>
+                                    <td><?php echo $user['email']; ?></td>
+                                    <td><?php echo $user['user_role']; ?></td>
+                                    <td>
+                                        <form action="" method="post" onsubmit="return confirmDelete();">
+                                            <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
+                                            <button type="submit" name="delete_userbtn" class="btn btn-danger mb-1">Delete</button>
+                                        </form>
+
+                                        <form action="update_user.php" method="post">
+                                            <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
+                                            <button class="btn btn-primary" type="submit" name="edit_userbtn">Edit</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+                    <!-- <button class="btn btn-primary">Print</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function confirmDelete() {
+        return confirm("Are you sure you want to delete this account?");
+    }
+</script>
+
+
+
+
+
+
+
+
+
+<?php include "pages/_footer.php" ?>
