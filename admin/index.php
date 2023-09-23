72cb94eb08d6c5417677e5762195cb928de7b7c3
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-09-23 15:55:11 +0300

    statistics are added to the admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,7 @@
 <?php include "pages/_header.php" ?>
 <?php include "pages/_navbar.php" ?>
+<?php include "../database/db_connection.php" ?>
+
 
 <div class="container mt-5 mb-5">
     <?php
@@ -13,13 +15,112 @@
     }
     ?>
 
-    <div class="row">
-        <div class="col">
+    <div class="row mt-5">
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-primary text-white mb-4">
+                <div class="card-body">Total Users
 
-        statisticsss
+                    <?php
+                    $query = "SELECT * FROM users";
+                    $query_run = mysqli_query($connection, $query);
+                    if ($total_users = mysqli_num_rows($query_run)) {
+                        echo '<h4 class="mb-0">' . $total_users . '</h4>';
+                    } else {
+                        echo '<h4 class="mb-0">No Data</h4>';
+                    }
+                    ?>
 
-
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="users_setting.php">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
         </div>
+
+
+
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-warning text-white mb-4">
+                <div class="card-body">Total Admin
+                    <?php
+                    $query = "SELECT * FROM users WHERE user_role='1'";
+                    $query_run = mysqli_query($connection, $query);
+                    if ($total_admin = mysqli_num_rows($query_run)) {
+                        echo '<h4 class="mb-0">' . $total_admin . '</h4>';
+                    } else {
+                        echo '<h4 class="mb-0">No Data</h4>';
+                    }
+                    ?>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="users_setting.php">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-success text-white mb-4">
+                <div class="card-body">Total Posts
+                    <?php
+                    $query = "SELECT * FROM posts";
+                    $query_run = mysqli_query($connection, $query);
+                    if ($total_posts = mysqli_num_rows($query_run)) {
+                        echo '<h4 class="mb-0">' . $total_posts . '</h4>';
+                    } else {
+                        echo '<h4 class="mb-0">No Data</h4>';
+                    }
+                    ?>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="posts.php">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-danger text-white mb-4">
+                <div class="card-body">Posts waiting for approve
+                    <?php
+                    $query = "SELECT status FROM posts WHERE status=0";
+                    $query_run = mysqli_query($connection, $query);
+                    if ($unvisible_posts = mysqli_num_rows($query_run)) {
+                        echo '<h4 class="mb-0">' . $unvisible_posts . '</h4>';
+                    } else {
+                        echo '<h4 class="mb-0">No Data</h4>';
+                    }
+                    ?>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="posts.php">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-danger text-white mb-4">
+                <div class="card-body">Approved posts
+                    <?php
+                    $query = "SELECT status FROM posts WHERE status=1";
+                    $query_run = mysqli_query($connection, $query);
+                    if ($visible_posts = mysqli_num_rows($query_run)) {
+                        echo '<h4 class="mb-0">' . $visible_posts . '</h4>';
+                    } else {
+                        echo '<h4 class="mb-0">No Data</h4>';
+                    }
+                    ?>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="posts.php">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </div>
 
